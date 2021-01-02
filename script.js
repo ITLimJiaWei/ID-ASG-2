180d37b12da16a30116a77919db65d36552dde4e
ITLimJiaWei <s10204971@connect.np.edu.sg>
2021-01-02 16:56:58 +0800

    Improved pages

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
-var settings = {
-    "url": "https://swapi.dev/api/people/1/",
-    "method": "GET",
-    "timeout": 0,
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+document.getElementById("btn").addEventListener("click",getpeople);
 
+
+function getpeople() {
+    var settings = {
+        "url": "https://swapi.dev/api/people/1/",
+        "method": "GET",
+        "timeout": 0,
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
     
-  });
+        
+      });
+
+      var name = document.getElementById("test").innerHTML = response[0].name ;
+}
+
+
+
