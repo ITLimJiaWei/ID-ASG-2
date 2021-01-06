2e902cc3146dbe86b8dd76d1af008226995527d3
ITLimJiaWei <s10204971@connect.np.edu.sg>
2021-01-06 18:33:46 +0800

    Added search function using SWAPI API

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var input = "people/1/"
-document.getElementById("btn").addEventListener("click",getpeople(input));
+document.getElementById("btn").addEventListener("click",function(event) {event.preventDefault(); button()});
+
+function button() {
+    var input = document.getElementById("name-input").value;
+    getpeople(input);
+    
+}
 
 
 function getpeople(input) {
     
     var settings = {
-        "url": "https://swapi.dev/api/" + input,
+        "url": "https://swapi.dev/api/people/?search="+input,
         "method": "GET",
         "timeout": 0,
       };
@@ -13,11 +18,18 @@ function getpeople(input) {
       $.ajax(settings).done(function (response) {
         console.log(response);
     
-        var name = document.getElementById("test").innerHTML = response.name;
-      });
+        var name = document.getElementById("name").innerHTML = response.results[0].name;
+        var height = document.getElementById("height").innerHTML = response.results[0].height+"cm";
+        var weight = document.getElementById("weight").innerHTML = response.results[0].mass+"kg";
+        var hair_colour = document.getElementById("hair-colour").innerHTML = response.results[0].hair_color;
+        var birth_year = document.getElementById("birth-year").innerHTML = response.results[0].birth_year;
 
-      
+      });
+    
 }
+
+
+
 
 
 
